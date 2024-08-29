8d89b9889f879582d138cdc2c27e06ec15218cd7
Alex007-Biz <bizukin@yandex.ru>
2024-08-29 08:55:49 +0300

    nav

--- FlowerDeliverySite/FlowerDeliverySite/urls.py
+++ FlowerDeliverySite/FlowerDeliverySite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 # from shop import views
@@ -24,4 +27,4 @@ urlpatterns = [
     # path('', views.index, name='index'),
     # path('order/', views.order, name='order'),
     path('', include('shop.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
